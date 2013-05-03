60f19a17de9511b47f541cb66c20097284661eba
Andreas Florath <Andreas.Florath@flonatel.de>
2013-05-03 15:35:17 +0200

    Added get / put to the object pool

--- lib/ptl/object_pool.hh
+++ lib/ptl/object_pool.hh
@@ -3,6 +3,9 @@
 
 #include <functional>
 #include <vector>
+#include <bitset>
+#include <memory>
+#include <stdexcept>
 
 namespace ptl { namespace object_pool {
 
@@ -22,6 +25,10 @@ public:
          objects.emplace_back( factory() );
       }
    }
+
+   static void no_more_objects( ) {
+      throw std::runtime_error( "No more objects" );
+   }
 };
 
 class alloc_new {
@@ -35,7 +42,7 @@ class multithread_block {
 template< typename ObjectType,
           int      PoolSize = 7,
           template< typename ObjectType, int PoolSize > class StrategyType
-          = strategies::fail /* < ObjectType, PoolSize >*/ >
+             = strategies::fail >
 class pool {
 public:
    using factory_type = std::function< ObjectType() >;
@@ -45,10 +52,48 @@ public:
       StrategyType< ObjectType, PoolSize >::init( _objects, _factory );
    }
 
+   ObjectType & get() {
+      int const first_free( search_first_free() );
+      if( first_free < 0 ) {
+         StrategyType< ObjectType, PoolSize >::no_more_objects();
+      }
+      alloc_object( first_free );
+      return _objects[ first_free ];
+   }
+
+   void put( ObjectType & obj ) {
+      for( int i{ 0 }; i < PoolSize; ++i ) {
+         if( & _objects[ i ] == & obj ) {
+            free_object( i );
+            return;
+         }
+      }
+      // Try to put an object which is not part of the pool.
+      abort();
+   }
+
 private:
+   int search_first_free() const {
+      for( int i{ 0 }; i < PoolSize; ++i ) {
+         if( not _used[i] ) {
+            return i;
+         }
+      }
+      return -1;
+   }
+
+   void alloc_object( int n ) {
+      _used.set( n );
+   }
+
+   void free_object( int n ) {
+      _used.reset( n );
+   }
+
    // ToDo: Move to alloc_new?
    factory_type const _factory;
-  std::vector< ObjectType > _objects;
+   std::vector< ObjectType > _objects;
+   std::bitset< PoolSize >   _used;
 };
 
 }}
--- tests/ObjectPoolTest.cc
+++ tests/ObjectPoolTest.cc
@@ -5,6 +5,10 @@
 class ObjectPoolTest : public ::testing::Test {
 public:
    void test_st_fail_init_only();
+   void test_st_fail_get_one();
+   void test_st_fail_get_all();
+   void test_st_fail_get_too_many();
+   void test_st_fail_put_many();
 };
 
 class A {
@@ -12,6 +16,8 @@ public:
    A( int i ) : _i( i ) {
       ++cnstr_call_cnt;
    }
+
+   int value() const { return _i; }
 
    static int cnstr_call_cnt;
 
@@ -26,9 +32,60 @@ A a_factory( int i ) {
 }
 
 TEST_F(ObjectPoolTest, test_st_fail_init_only) {
-
+   A::cnstr_call_cnt = 0;
    int const j { 9 };
    ptl::object_pool::pool< A > a_pool( std::bind( a_factory, j ) );
+   ASSERT_EQ( A::cnstr_call_cnt, 7 );
+}
+
+TEST_F(ObjectPoolTest, test_st_fail_get_one) {
+   A::cnstr_call_cnt = 0;
+   int const j { 9 };
+   ptl::object_pool::pool< A > a_pool( std::bind( a_factory, j ) );
+
+   A & a( a_pool.get() );
+
+   ASSERT_EQ( A::cnstr_call_cnt, 7 );
+   ASSERT_EQ( a.value(), 9 );
+}
+
+TEST_F(ObjectPoolTest, test_st_fail_get_all) {
+   A::cnstr_call_cnt = 0;
+   int const j { 9 };
+   ptl::object_pool::pool< A > a_pool( std::bind( a_factory, j ) );
+
+   for( int i{ 0 }; i < 7; ++i ) {
+      A & a( a_pool.get() );
+      ASSERT_EQ( a.value(), 9 );
+   }
+
+   ASSERT_EQ( A::cnstr_call_cnt, 7 );
+}
+
+TEST_F(ObjectPoolTest, test_st_fail_get_too_many) {
+   A::cnstr_call_cnt = 0;
+   int const j { 9 };
+   ptl::object_pool::pool< A > a_pool( std::bind( a_factory, j ) );
+
+   for( int i{ 0 }; i < 7; ++i ) {
+      A & a( a_pool.get() );
+      ASSERT_EQ( a.value(), 9 );
+   }
+
+   ASSERT_EQ( A::cnstr_call_cnt, 7 );
+   ASSERT_THROW( a_pool.get(), std::runtime_error );
+}
+
+TEST_F(ObjectPoolTest, test_st_fail_put_many) {
+   A::cnstr_call_cnt = 0;
+   int const j { 9 };
+   ptl::object_pool::pool< A > a_pool( std::bind( a_factory, j ) );
+
+   for( int i{ 0 }; i < 15; ++i ) {
+      A & a( a_pool.get() );
+      ASSERT_EQ( a.value(), 9 );
+      a_pool.put( a );
+   }
 
    ASSERT_EQ( A::cnstr_call_cnt, 7 );
 }
